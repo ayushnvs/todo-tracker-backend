d5f5222a8751e4c9b5b648056e51614b9b9af550
ayushnvs <ayush.mishra.ald@gmail.com>
2023-06-03 23:28:19 +0530

    updates

--- routes/tasks.js
+++ routes/tasks.js
@@ -54,4 +54,28 @@ router.route('/update/:id').post((req, res) => {
         })
 })
 
+// Add Updates
+router.route('/update/updates/:id').post((req, res) => {
+    Task.findById(req.params.id)
+        .then(task => {
+            if (req.body.updates !== '') task.updates.push(req.body.updates)
+
+            task.save()
+                .then(() => res.json('Updates Added!!'))
+                .catch(err => res.status(400).json('Error: ' + err))
+        })
+})
+
+// Update Status
+router.route('/update/status/:id').post((req, res) => {
+    Task.findById(req.params.id)
+        .then(task => {
+            task.status = req.body.status
+
+            task.save()
+                .then(() => res.json('Status Updated!'))
+                .catch(err => res.status(400).json('Error: ' + err))
+        })
+})
+
 module.exports = router
